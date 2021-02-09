d51a3c9b278f7727869d8fd872cd164177c15fe0
Dirk Kelderman <dirkkelderman@gmail.com>
2021-02-09 19:59:20 +0100

    front-end but tasks not working fine

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import ProjectList from './components/projects/ProjectList'
+import {Switch, Route} from 'react-router-dom'
+import ProjectDetails from './components/projects/ProjectDetails';
+import AddProject from './components/projects/AddProject';
+import TaskDetails from './components/tasks/TaskDetails'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Switch>
+      {/* <Route path='/' component={AddProject} /> */}
+      <Route exact path='/projects' component={ProjectList} />
+      <Route exact path='/projects/:id' component={ProjectDetails} />
+      <Route exact path="/projects/:id/tasks/:taskId" component={TaskDetails} /> {/* <== !!! */}
+
+    </Switch>
     </div>
   );
 }
---
+++ src/components/projects/AddProject.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+import axios from 'axios'
+
+export class AddProject extends Component {
+
+    state = {
+        title: '',
+        description: '',
+        status: ''
+    }
+
+    handleFormSubmit = (event) => {
+        event.preventDefault();
+
+        axios.post('http://localhost:5000/api/projects', {
+            title: this.state.title,
+            description: this.state.description
+        })
+        .then( (res) => {
+                this.props.getData();
+                    this.setState({
+                        title: '',
+                        description: '',
+                        status: 'Your project is created'
+                    })
+                }, (err) => {
+                    console.log(err)
+                    this.setState({
+                        status: 'Oops, something wrong'
+                    })
+                })
+        
+        // fetch('http://localhost:5000/api/projects', {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type' : 'application/json',
+        //     },
+        //     body: JSON.stringify(this.state)
+        // })
+        // .then( res => res.json() )
+        // .then( (res) => {
+        //     console.log("Request succesfull")
+        //     console.log(res)
+        // }, (err) => {
+        //     console.log("Error!")
+        //     console.log(err)
+        // })
+
+    }
+
+    handleChange = (event) => {  
+        const {name, value} = event.target;
+        this.setState({[name]: value});
+    }
+   
+    render(){
+      return(
+        <div>
+        {
+            this.state.status !== '' ? <div>{this.state.status}</div> : null
+        }
+          <form onSubmit={this.handleFormSubmit}>
+            <label>Title:</label>
+            <input type="text" name="title" value={this.state.title} onChange={ e => this.handleChange(e)}/>
+            <label>Description:</label>
+            <textarea name="description" value={this.state.description} onChange={ e => this.handleChange(e)} />
+            
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
+      )
+    }
+}
+
+export default AddProject
